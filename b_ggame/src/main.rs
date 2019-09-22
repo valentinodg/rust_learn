a9a27c7f91ede2ca219d6116a242cc128238cda6
valentinodg <valentino.digiosaffatte@student.univaq.it>
2019-09-23 01:07:05 +0200

    .

---
+++ b_ggame/src/main.rs
@@ -0,0 +1,35 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main(){
+    
+    println!("guess the number!");
+    
+    let secret_number = rand::thread_rng().gen_range(1,101);
+
+    loop {
+        println!("pls input the guess");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("failed to read line");
+
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue
+        };
+            
+        println!("you guessed: {}", guess);
+
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("too small"),
+            Ordering::Greater => println!("too big"),
+            Ordering::Equal => {
+                println!("YOU WIN");
+                break;
+            }
+        }
+    }
+}
