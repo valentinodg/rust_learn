cd5f7105f669d85e993b30ed9a1c6fd33fc41931
valentinodg <valentino.digiosaffatte@student.univaq.it>
2019-10-28 21:18:19 +0100

    .

--- h2_collections_strings/src/main.rs
+++ h2_collections_strings/src/main.rs
@@ -1,70 +1,75 @@
 fn main() {
-    //strings are implemented as a collection of bytes plus some methods
-    //to provide useful functionality when those bytes are interpreted as
-    //text
+    // STORING UTF-8 ENCODED TEXT WITH STRINGS
 
-    //rust has one string type in the core language which is the string slice
-    //str that is usually seen in its borrowed form &str
-    //we talked about string slices which are references to some UTF-8 encoded
-    //string data store elsewhere
+    // strings are implemented as a collection of bytes plus some methods
+    // to provide useful functionality when those bytes are interpreted as
+    // text
 
-    //the String type which is provided by rust's std lib rather than coded into
-    //the core language, is a growable, mutable, owned, UTF-8 encoded string type
-    //when rustaceans refer to "strings" in rust, they usually mean the String
-    //and the string slice &str types
+    // WHAT IS A STRING?
 
-    //rust's std lib also includes a number of other string types such as 0sString,
-    //0sStr, CString, CStr and also library crates can provide even more options
-    //for storing string data
-    //the names that ends with String refers to the owned variants and the names
-    //that ends with Str refers to the borrowed variants
+    // rust has one string type in the core language which is the string slice
+    // str that is usually seen in its borrowed form &str
+    // we talked about string slices which are references to some UTF-8 encoded
+    // string data store elsewhere
 
-    //create a new string
-    //many of the same operations available with Vec<T> are available with String
-    //as well
-    //let's use the new function to create a string
+    // the String type which is provided by rust's std lib rather than coded into
+    // the core language, is a growable, mutable, owned, UTF-8 encoded string type
+    // when rustaceans refer to "strings" in rust, they usually mean the String
+    // and the string slice &str types
+
+    // rust's std lib also includes a number of other string types such as 0sString,
+    // 0sStr, CString, CStr and also library crates can provide even more options
+    // for storing string data
+    // the names that ends with String refers to the owned variants and the names
+    // that ends with Str refers to the borrowed variants
+
+    // CREATING A NEW STRING
+
+    // many of the same operations available with Vec<T> are available with String
+    // as well
+    // let's use the new function to create a string
 
     let _s = String::new();
 
-    //this line creates a new empty string called s
-    //let's use the method to_string() to initialize the string variables
-    //(to_string() method is available on any type that implements the Display
-    //trait)
+    // this line creates a new empty string called s
+    // let's use the method to_string() to initialize the string variables
+    // (to_string() method is available on any type that implements the Display
+    // trait)
 
     let data = "initial contents";
 
     let s = data.to_string();
     let s1 = "initial contents".to_string();
-    //the code creates a string containing "initial contents"
-    //we can also use the function String::from to create String from a string
-    //literal
+    // the code creates a string containing "initial contents"
+    // we can also use the function String::from to create String from a string
+    // literal
 
     let s2 = String::from("initial contents");
-    //because strings are UFT-8 encoded we can include any properly encoded data
-    //in them
+    // because strings are UFT-8 encoded we can include any properly encoded data
+    // in them
 
     println!(
         "\ns: \"{}\"\ndata: \"{}\"\ns1: \"{}\"\ns2: \"{}\"",
         s, data, s1, s2
     );
 
-    //updating a string
+    // UPDATING A STRING
 
-    //a String can grow in size and its contents can change, just like the
-    //contents of a Vec<T>, if you push more data into it
-    //in additions you can use the + operator or the format! macro to concatenate
-    //String values
+    // a String can grow in size and its contents can change, just like the
+    // contents of a Vec<T>, if you push more data into it
+    // in additions you can use the + operator or the format! macro to concatenate
+    // String values
 
-    //appending to a string with push_str and push
+    // APPENDING TO A STRING WITH PUSH_STR AND PUSH
 
-    //you can grow a String by using the push_str() method to append a string slice
+    // you can grow a String by using the push_str() method to append a string slice
 
     let mut s3 = String::from("foo");
     s3.push_str("bar");
 
     println!("\ns3: \"{}\"", s3);
-    //the push_str() method takes a string slice because we don't necessarily want
-    //to take ownership of the parameter
+    // the push_str() method takes a string slice because we don't necessarily want
+    // to take ownership of the parameter
 
     let mut s4 = String::from("foo");
     let s5 = "bar";
@@ -72,17 +77,17 @@ fn main() {
 
     println!("s4: \"{}\"s5: \"{}\"", s4, s5);
 
-    //the push() method takes a single character as a parameter and adds it to the
-    //String
+    // the push() method takes a single character as a parameter and adds it to the
+    // String
 
     let mut s6 = String::from("lo");
     s6.push('l');
 
     println!("s6: \"{}\"", s6);
 
-    //concatenation with the + operator or the format! macro
+    // CONCATENATION WITH THE + OPERATOR OR THE FORMAT! MACRO
 
-    //if we want to combine 2 existing strings, one way is to use the + operator
+    // if we want to combine 2 existing strings, one way is to use the + operator
 
     let _string0 = String::from("hello ");
     let _string1 = String::from("world!");
@@ -91,30 +96,30 @@ fn main() {
 
     println!("\nstring2: \"{}\"", string2);
 
-    //string2 will contain "hello world!" as a result of this code
-    //the reason string0 is no longer valid after the addition and the reason we used
-    //a reference to string1 has to do with the signature of the method that gets
-    //called when we use the + operator
-    //
-    //the + operator uses the add method, whose signature looks something like this:
-    //fn add(self, s: &str) -> String {}
-    //(this isn't the exact signature that's in the std lib, because in std add is
-    //defined using generics)
-    //
-    //the second parameter has an &, meaning that we're adding a reference of the
-    //second string to the first string
-    //we can only add a &str to a String
-    //but we used a &String value as second parameter and not &str, why rustc compiles?
-    //the reason we're able to use &string1 in the call to add is that the compiler
-    //can coerce the &String argument into a &str
-    //when we call the add method rust uses a deref coercion which here turns &sring1
-    //into &string1[..]
-    //
-    //the implementation is more efficient than copying
+    // string2 will contain "hello world!" as a result of this code
+    // the reason string0 is no longer valid after the addition and the reason we used
+    // a reference to string1 has to do with the signature of the method that gets
+    // called when we use the + operator
+    // 
+    // the + operator uses the add method, whose signature looks something like this:
+    // fn add(self, s: &str) -> String {}
+    // (this isn't the exact signature that's in the std lib, because in std add is
+    // defined using generics)
+    // 
+    // the second parameter has an &, meaning that we're adding a reference of the
+    // second string to the first string
+    // we can only add a &str to a String
+    // but we used a &String value as second parameter and not &str, why rustc compiles?
+    // the reason we're able to use &string1 in the call to add is that the compiler
+    // can coerce the &String argument into a &str
+    // when we call the add method rust uses a deref coercion which here turns &sring1
+    // into &string1[..]
+    // 
+    // the implementation is more efficient than copying
 
-    //multiple concatenation
+    // multiple concatenation
 
-    //using the + operator
+    // using the + operator
     let s7 = String::from("tic");
     let s8 = String::from("tac");
     let s9 = String::from("toe");
@@ -123,7 +128,7 @@ fn main() {
 
     println!("\nfs: \"{}\"", fs);
 
-    //using the format! macro
+    // using the format! macro
     let s72 = String::from("tic");
     let s82 = String::from("tac");
     let s92 = String::from("toe");
@@ -131,82 +136,82 @@ fn main() {
     let fs2 = format!("{} {} {}", s72, s82, s92);
 
     println!("fs2: \"{}\"", fs2);
-    //the format! macro works in the same way as println!
-    //it returns a String with contents
-    //the version of code using format! is much easier to read and doesn't take
-    //ownership of any of its parameters
+    // the format! macro works in the same way as println!
+    // it returns a String with contents
+    // the version of code using format! is much easier to read and doesn't take
+    // ownership of any of its parameters
 
-    //indexing into Strings
+    // INDEXING INTO STRINGS
 
-    //in many other programming languages, accessing individual characters in a string
-    //by referencing them by index is a valid and common operation
-    //however, if you try to access aprts of a String using indexing syntax in
-    //tust, you'll get an error
-    //rust strings don't support indexing because of way that rust stores strings
-    //in memory
+    // in many other programming languages, accessing individual characters in a string
+    // by referencing them by index is a valid and common operation
+    // however, if you try to access aprts of a String using indexing syntax in
+    // tust, you'll get an error
+    // rust strings don't support indexing because of way that rust stores strings
+    // in memory
 
-    //internal representation
+    // INTERNAL REPRESENTATION
 
-    //a String is a wrapper over a Vec<u8>
+    // a String is a wrapper over a Vec<u8>
     let len = String::from("hello").len();
     println!("\nlen: {}", len);
-    //in this case len will be 5 which means the vector storing the string "hello"
-    //is 5 bytes long
-    //in this case every letter takes 1 byte when encoded in UTF-8
+    // in this case len will be 5 which means the vector storing the string "hello"
+    // is 5 bytes long
+    // in this case every letter takes 1 byte when encoded in UTF-8
 
     let len2 = String::from("Здравствуйте").len();
     println!("len2: {}", len2);
-    //in this case each unicode scalar value in that string takes 2 bytes of storage
-    //therefore an index into the string's bytes will not always correlate to a
-    //valid unicode scalar value
+    // in this case each unicode scalar value in that string takes 2 bytes of storage
+    // therefore an index into the string's bytes will not always correlate to a
+    // valid unicode scalar value
 
-    //to avoid returning unexpected values and causing bugs taht might not be discovered
-    //immediately, rust doesn't compile the following code
-    //
-    //let hello = "hello";
-    //let letterh = &hello[0];
+    // to avoid returning unexpected values and causing bugs taht might not be discovered
+    // immediately, rust doesn't compile the following code
+    // 
+    // let hello = "hello";
+    // let letterh = &hello[0];
 
-    //bytes and scalar values and grapheme clusters
+    // BYTES AND SCALAR VALUES AND GRAPHEME CLUSTERS
 
-    //rust looks at strings in 3 ways: as bytes, scalar values and grapheme clusters
-    //
-    //let's traduce the Hindi word “नमस्ते” representation in bytes, scalar values
-    //and grapheme clusters
-    //
-    //(in bytes -> vector of u8 values)
-    //[224, 164, 168, 224, 164, 174, 224, 164, 184, 224, 165, 141, 224, 164, 164,
-    //224, 165, 135]
-    //
-    //(in unicode scalar values -> rust's char type)
-    //['न', 'म', 'स', '्', 'त', 'े']
-    //
-    //(in grapheme clusters -> human readable letters)
-    //["न", "म", "स्", "ते"]
+    // rust looks at strings in 3 ways: as bytes, scalar values and grapheme clusters
+    // 
+    // let's traduce the Hindi word “नमस्ते” representation in bytes, scalar values
+    // and grapheme clusters
+    // 
+    // (in bytes -> vector of u8 values)
+    // [224, 164, 168, 224, 164, 174, 224, 164, 184, 224, 165, 141, 224, 164, 164,
+    // 224, 165, 135]
+    // 
+    // (in unicode scalar values -> rust's char type)
+    // ['न', 'म', 'स', '्', 'त', 'े']
+    // 
+    // (in grapheme clusters -> human readable letters)
+    // ["न", "म", "स्", "ते"]
 
-    //a final reason rust doesn't allow us to index into a String to get a characters
-    //is that indexing operations are expected to always take constant time O(1)
-    //but it isn't possible to guarantee that performance with a String because rust
-    //would have to walk through the contents from the beginning to the index to
-    //determine how many valid characters there were
+    // a final reason rust doesn't allow us to index into a String to get a characters
+    // is that indexing operations are expected to always take constant time O(1)
+    // but it isn't possible to guarantee that performance with a String because rust
+    // would have to walk through the contents from the beginning to the index to
+    // determine how many valid characters there were
 
-    //slicing strings
+    // SLICING STRINGS
 
-    //indexing into a string is ofter a bad idea because it's not clear that the return
-    //type of the string-indexing operations should be: a byte value, a character,
-    //a grapheme cluster, or a string slice
-    //if you want to use a string slice you need to be more precise in your indexing
-    //and use [] with a range to create a string slice containing particular bytes
+    // indexing into a string is ofter a bad idea because it's not clear that the return
+    // type of the string-indexing operations should be: a byte value, a character,
+    // a grapheme cluster, or a string slice
+    // if you want to use a string slice you need to be more precise in your indexing
+    // and use [] with a range to create a string slice containing particular bytes
 
     let sstring = "Здравствуйте";
     let stringslice = &sstring[0..4];
     println!("\nstringslice: \"{}\"\n", stringslice);
-    //if we use &sstring[0..1] rust would panic at runtime because of the 2 bytes per
-    //letter representation that we explain before
+    // if we use &sstring[0..1] rust would panic at runtime because of the 2 bytes per
+    // letter representation that we explain before
 
-    //methods for iterating over strings
+    // METHODS FOR ITERATING OVER STRINGS
 
-    //if you need to perform operations on individual unicode scalar values (access
-    //elements in a string) you can use the chars method (best way)
+    // if you need to perform operations on individual unicode scalar values (access
+    // elements in a string) you can use the chars method (best way)
 
     for c in "Здрав".chars() {
         print!("{} ", c);
@@ -214,15 +219,15 @@ fn main() {
 
     println!("");
 
-    //the bytes method returns each raw byte of the string
+    // the bytes method returns each raw byte of the string
     for c in "Здрав".bytes() {
         print!("{} ", c);
     }
 
     println!("");
 
-    //remember that unicode scalar values may be made up of more than 1 byte
-    //getting grapheme clusters from strings is complex so this functionality is not
-    //provided by the std lib (look at crates available on crates.io for this
-    //functionality)
+    // remember that unicode scalar values may be made up of more than 1 byte
+    // getting grapheme clusters from strings is complex so this functionality is not
+    // provided by the std lib (look at crates available on crates.io for this
+    // functionality)
 }
--- h3_collections_hashmaps/src/main.rs
+++ h3_collections_hashmaps/src/main.rs
@@ -1,14 +1,15 @@
 fn main() {
-    //storing keys with associated values in hash maps
+    // STORING KEYS WITH ASSOCIATED VALUES IN HASH MAPS
 
-    //the type HashMap<K,V> stores a mapping of keys of type K to values of type V.
-    //it does this via a hashing function that determines how it places there keys and values
-    //into memory
-    //hash maps  are useful when you want to look up data not by using an index, as you can do with
-    //vectors, but by using a key that can be of any type
+    // the type HashMap<K,V> stores a mapping of keys of type K to values of type V.
+    // it does this via a hashing function that determines how it places there keys and values
+    // into memory
+    // hash maps  are useful when you want to look up data not by using an index, as you can do with
+    // vectors, but by using a key that can be of any type
 
-    //creating a new hash map
-    //you can create an empty hash map with new and add elements with insert
+    // CREATING A NEW HASH MAP
+
+    // you can create an empty hash map with new and add elements with insert
 
     use std::collections::HashMap;
 
@@ -18,22 +19,22 @@ fn main() {
     scoresx.insert(String::from("yellow"), 50);
 
     println!("\n{:?}", scoresx);
-    //note that we need to first use the HashMap from the collections portion of the std lib
-    //because of our 3 common collections, this one is the least ofter used so it's not included
-    //int the features brought into scope automatically in the prelude
-    //hash maps also have less support from the std lib (for example there's no built-in macro
-    //to construct them)
+    // note that we need to first use the HashMap from the collections portion of the std lib
+    // because of our 3 common collections, this one is the least ofter used so it's not included
+    // int the features brought into scope automatically in the prelude
+    // hash maps also have less support from the std lib (for example there's no built-in macro
+    // to construct them)
 
-    //just like vectors, hash maps store their data on the heap
-    //like vectors, hash maps are homogeneous: all of the keys must have the same type and all of
-    //the values must have the same type
+    // just like vectors, hash maps store their data on the heap
+    // like vectors, hash maps are homogeneous: all of the keys must have the same type and all of
+    // the values must have the same type
 
-    //another way of constructing a hash map is by using the collect method on a vector of tuples
-    //(where each tuple consists of a key and its value)
-    //the collect method gathers data into a number of collection types, including hash maps
+    // another way of constructing a hash map is by using the collect method on a vector of tuples
+    // (where each tuple consists of a key and its value)
+    // the collect method gathers data into a number of collection types, including hash maps
 
-    //if we have a vector of keys and a vector of values, we can use the zup method to create a
-    //vector of tuples and then we can use the collect method to create our hash map
+    // if we have a vector of keys and a vector of values, we can use the zup method to create a
+    // vector of tuples and then we can use the collect method to create our hash map
 
     let teams = vec![String::from("blue"), String::from("yellow")];
     let initial_scores = vec![10, 50];
@@ -41,16 +42,16 @@ fn main() {
     let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
 
     println!("\n{:?}", scores);
-    //the type annotation HashMap<_,_> is needed here because it's possible to collect into many
-    //different data structures and rust doesn't know wichi you want unless you specify
-    //for the paramenters for the key and values types we use _ and rust can infer the types that
-    //the hash map contains based on the types of the data in the vectors
+    // the type annotation HashMap<_,_> is needed here because it's possible to collect into many
+    // different data structures and rust doesn't know wichi you want unless you specify
+    // for the paramenters for the key and values types we use _ and rust can infer the types that
+    // the hash map contains based on the types of the data in the vectors
 
-    //hash maps and ownership
+    // HASH MAPS AND OWNERSHIP
 
-    //for types that implements the copy trait, like i32, the values are copied into the hash map
-    //for owned values like String, the values will be moved and the hash map be the owner of
-    //those values
+    // for types that implements the copy trait, like i32, the values are copied into the hash map
+    // for owned values like String, the values will be moved and the hash map be the owner of
+    // those values
 
     let field_name = String::from("color");
     let field_value = String::from("blue");
@@ -59,28 +60,28 @@ fn main() {
     map.insert(field_name, field_value);
 
     println!("\n{:?}", map);
-    //we aren't able to use the variables field_name and field_value after they've been moved into
-    //the hash map with the call to insert
-    //if we insert references to values into the hash map, tha values won't be moved into the hash
-    //map
-    //the values that the references point to must be valued for at least as long as the hash map
-    //is valid ("validating references with lifetimes")
+    // we aren't able to use the variables field_name and field_value after they've been moved into
+    // the hash map with the call to insert
+    // if we insert references to values into the hash map, tha values won't be moved into the hash
+    // map
+    // the values that the references point to must be valued for at least as long as the hash map
+    // is valid ("validating references with lifetimes")
 
-    //accessing values in hash map
+    // ACCESSING VALUES IN HASH MAP
 
-    //we can get a value out of the hash map by providing its key to the get method
+    // we can get a value out of the hash map by providing its key to the get method
 
     let team_name = String::from("blue");
     let score2 = scores.get(&team_name);
 
     println!("\n{:?}\n", score2);
-    //here, score2 will have the value that's associated with the blue key and the result will be
-    //Some(&10)
-    //the result is wrapped in Some because get returns an Option(&V) or None (if there's no value
-    //for that key in the hash map)
+    // here, score2 will have the value that's associated with the blue key and the result will be
+    // Some(&10)
+    // the result is wrapped in Some because get returns an Option(&V) or None (if there's no value
+    // for that key in the hash map)
 
-    //we can iterate over each key/value pair in a hash map in a similar manner as we fo with
-    //vectors, using a for loop
+    // we can iterate over each key/value pair in a hash map in a similar manner as we fo with
+    // vectors, using a for loop
     let mut scores2 = HashMap::new();
 
     scores2.insert(String::from("blue"), 10);
@@ -90,15 +91,15 @@ fn main() {
         println!("{}: {}", key, value);
     }
 
-    //updating a hash map
+    // UPDATING A HASH MAP
 
-    //although the number of keys and values is growable, each key can only have 1 value
-    //associated with it at a time
+    // although the number of keys and values is growable, each key can only have 1 value
+    // associated with it at a time
 
-    //overwriting a value
+    // OVERWRITING A VALUE
 
-    //if we insert a key and a value into a hash map and then insert that same key with a
-    //differrent value, the values associated with that key will be replaced
+    // if we insert a key and a value into a hash map and then insert that same key with a
+    // differrent value, the values associated with that key will be replaced
 
     let mut scr = HashMap::new();
 
@@ -107,14 +108,14 @@ fn main() {
 
     println!("\n{:?}", scr);
 
-    //inserting a value if the key has no value
+    // INSERTING A VALUE IF THE KEY HAS NO VALUE
 
-    //it's common to check whether a particular key has a value and, if it doesn't, insert a value
-    //for it
-    //hash maps have a special API for this called entry that takes the key you want to check as a
-    //parameter
-    //the return value of the entry method is an enum called Entry that represents a value that
-    //might or might not exist
+    // it's common to check whether a particular key has a value and, if it doesn't, insert a value
+    // for it
+    // hash maps have a special API for this called entry that takes the key you want to check as a
+    // parameter
+    // the return value of the entry method is an enum called Entry that represents a value that
+    // might or might not exist
 
     let mut scr2 = HashMap::new();
     scr2.insert(String::from("blue"), 10);
@@ -123,16 +124,16 @@ fn main() {
     scr2.entry(String::from("blue")).or_insert(50);
 
     println!("\n{:?}", scr2);
-    //the or_insert method on Entry is defined to return a mutable reference to the value for the
-    //corresponding Entry key if that key exist, and if not, inserts the parameter as the new
-    //values for this key and returns a mutable reference to the new value
-    //this technique is much cleaner than writing the logic ourselves and, in addition, plays more
-    //nicely with the borrow checker
+    // the or_insert method on Entry is defined to return a mutable reference to the value for the
+    // corresponding Entry key if that key exist, and if not, inserts the parameter as the new
+    // values for this key and returns a mutable reference to the new value
+    // this technique is much cleaner than writing the logic ourselves and, in addition, plays more
+    // nicely with the borrow checker
 
-    //updating a value based on the old value
+    // UPDATING A VALUE BASED ON THE OLD VALUE
 
-    //another common use case for hash maps is to look a key's value and then update it based on
-    //the old value
+    // another common use case for hash maps is to look a key's value and then update it based on
+    // the old value
 
     let text = "hello world wonderfull world";
 
@@ -144,12 +145,14 @@ fn main() {
     }
 
     println!("\n{:?}", map2);
-    //the or_insert method actually returns a mutable reference (&mut V) to the value for this key
+    // the or_insert method actually returns a mutable reference (&mut V) to the value for this key
 
-    //by default HashMap uses a cryptographically strong hashing function that can provide
-    //resistance to DoS attacks
-    //this is not the fastest hashing algorithm available, but the trade-off for better security
-    //that comes  with the drop in performance is worth it
-    //you can specify a difference hasher (look at libs in craters.io
-    //a hasher is a type that implements the BuildHasher trait
+    // HASHING FUNCTIONS 
+    
+    // by default HashMap uses a cryptographically strong hashing function that can provide
+    // resistance to DoS attacks
+    // this is not the fastest hashing algorithm available, but the trade-off for better security
+    // that comes  with the drop in performance is worth it
+    // you can specify a difference hasher (look at libs in craters.io
+    // a hasher is a type that implements the BuildHasher trait
 }
